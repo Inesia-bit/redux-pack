c97102b2a554fc52d2b4254a92f46a16b4f120ec
Agnes Ndip <speak2jedi@gmail.com>
2021-10-07 07:36:10 +0100

    components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
 
 function App() {
-  return <div className="App">madness</div>;
+  return (
+    <div className="App">
+      <Login />
+      <Profile />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div>
+      <h1>Profile</h1>
+      <p>Name:</p>
+      <p>Age:</p>
+      <p>Sex:</p>
+    </div>
+  );
+};
+
+export default Profile;
